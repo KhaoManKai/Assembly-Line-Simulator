efc1c82f24f0c8d2346cc819e9dd974c57cae9b8
KhaoManKai <sammullee.123@gmail.com>
2025-04-06 20:16:44 -0400

    Update Station.cpp

--- WS4/Station.cpp
+++ WS4/Station.cpp
@@ -1,10 +1,4 @@
-// Workshop 4
-//
-// -----------------------------------------------------------
-// Name Hin Lum Lee     Date 10 Dec 2024   ID 132957234
-// NBB  hllee13@myseneca.ca
-//I declare that this submission is the result of my own work and I only copied the code that my professor provided to complete my workshops and assignments. This submitted piece of work has not been shared with any other student or 3rd party content provider.
-/////////////////////////////////////////////////////////////////
+
 #include "Station.h"
 #include <iomanip>
 namespace seneca{
